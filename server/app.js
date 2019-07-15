5288244196f7a756771e316abbb31b697ba817a6
Oiver Yan Li <oliverlee1003@outlook.com>
2019-07-15 00:13:50 -0700

    add pacakge file in root

--- server/app.js
+++ server/app.js
@@ -32,6 +32,13 @@ app.use("/testAPI", testAPIRouter);
 app.use('/messages', messagesRouter);
 
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('../client/build'));
+  app.get('*',(req,res)=> {
+    res.sendFile(path.join(__dirname,'client','build','index.html')); //relative path
+  });
+}
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
